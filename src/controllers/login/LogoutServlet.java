4c49357c233cb1d627e7e81bf0190611ae7ce110
yuka.tojima <tjm.419@ezweb.ne.jp>
2020-12-25 07:25:24 +0900

    Add LoginFilter

--- src/controllers/login/LogoutServlet.java
+++ src/controllers/login/LogoutServlet.java
@@ -31,6 +31,7 @@ public class LogoutServlet extends HttpServlet {
     request.getSession().removeAttribute("login_employee");
 
     request.getSession().setAttribute("flush", "ログアウトしました。");
+    response.sendRedirect(request.getContextPath() + "/login");
     }
 
 }
